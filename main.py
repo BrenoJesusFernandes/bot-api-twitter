3a377f4d0e9b1439dcb38504cf89bff2b76cfe1c
Breno Fernandes <brenojesusfernandes@pm.com>
2022-05-19 23:40:31 -0230

    Feature: Realizando busca e retweitando

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
+import time
+
 import tweepy
+from tweepy import Response, Tweet
 from loguru import logger
+from typing import List
 
 
 class TwitterBot:
 
-    def __init__(self, api: str, api_secret: str, token: str, token_secrect: str):
-        print(api, api_secret, token, token_secrect)
-
+    def __init__(self, api: str, api_secret: str, token: str, token_secrect: str, bearer_token: str):
         self.client = tweepy.Client(consumer_key=api,
                                     consumer_secret=api_secret,
                                     access_token=token,
-                                    access_token_secret=token_secrect)
+                                    access_token_secret=token_secrect,
+                                    bearer_token=bearer_token,
+                                    wait_on_rate_limit=True)
 
     def __on_mention(self, tweet, prefix):
         """
@@ -28,18 +32,17 @@ class TwitterBot:
         ...
 
     def run(self):
-        """
-        Runs the bot!
-        """
         while True:
+            try:
+                # Check mention
+                self.__check_mentions()
 
-            # Check mention
-            self.__check_mentions()
+                # Retweet
+                self.client.retweet('user_id')
 
-            # Retweet
-            self.client.retweet('user_id')
-
-            ...
+            except Exception as error:
+                logger.error(error)
+                time.sleep(60)
 
 
 @logger.catch
@@ -47,14 +50,25 @@ def run():
     twitter_bot = TwitterBot(os.getenv('API_KEY'),
                              os.getenv('API_KEY_SECRET'),
                              os.getenv('ACCESS_TOKEN'),
-                             os.getenv('ACCESS_TOKEN_SECRET'))
+                             os.getenv('ACCESS_TOKEN_SECRET'),
+                             os.getenv('BEARER_TOKEN'))
 
-    resp = twitter_bot.client.create_tweet(text=r'*beep*')
+    query = '-from:BolhaDados -is:retweet ((#BolhaDados) OR (@BolhaDados))'
+
+    resp: Response = twitter_bot.client.search_recent_tweets(query=query, max_results=10)
     print(resp)
+    tweets: List[Tweet] = resp.data
+
+    if tweets is not None:
+        print(len(tweets))
+        for tweet in tweets:
+            print('Retwitand id:', tweet.data)
+            twitter_bot.client.retweet(tweet.id)
+            twitter_bot.client.like(tweet.id)
+
 
     # twitter_bot.run()
 
 
 if __name__ == '__main__':
-    logger.add('log/error.log')
     run()
